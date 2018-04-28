bfd64154e5c8b008c7a649fe3260fb2b9fcfd830
BRAUGHTON <bec.braughton@gmail.com>
2018-04-27 21:36:23 -0500

    Ben's code - worked for him, not working now. Will reasess.

---
+++ Battleship.py
@@ -0,0 +1,26 @@
+from random import randint
+import numpy as np
+
+class Player:
+    def _init_(self):
+        self.ship_board = [[0 for x in range(8)] for y in range(8)]
+        self.hit_board = [[0 for x in range(8)] for y in range(8)]
+
+def print_board(board):
+    for row in board: 
+        pring " ".join(row)
+
+player1 = Player()
+player2 = Player()
+
+player1.ship_board[0] [0] = 1
+
+print (np.matrix(player1.ship_board))
+
+guess_row = int(raw_input("Guess Row: "))
+guess_col = int(raw_input("Guess Column: "))
+
+if player1.ship_board[guess_row][guess_col] == 1:
+    print ("You did it!")
+else:
+    print ("Failure.")
